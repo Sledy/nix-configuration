e9d133d2e959cd114b7e5bdb01d36ab1d4678ea2
Adam Śledziewski <sledziewski.adam@proton.me>
2025-05-18 16:23:22 +0200

    Power management set to false, add prime options, finegrained set to true

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -16,6 +16,7 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
+    extraPackages = with pkgs; [nvidia-vaapi-driver];
   };
 
   # Load nvidia driver for Xorg and Wayland
@@ -31,7 +32,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = true;
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -51,6 +52,12 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.production;
+    
+    prime = {
+    	offload.enable = true;
+    	intelBusId = "PCI:0:2:0";
+    	nvidiaBusId = "PCI:1:0:0";
+    }
   };
 
   # ----------- ENDING SECTION NVIDIA graphics ------------------
