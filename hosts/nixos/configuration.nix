69d0aecaf914a65f2bb7f627081cb3f9ae805da6
Adam Śledziewski <sledziewski.adam@proton.me>
2025-05-18 16:50:57 +0200

    Change Nvidia driver version to beta

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -58,7 +58,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     
     prime = {
     	offload.enable = true;
