65c12d6912c11656f2b653f998444bb89c6cb042
Adam Śledziewski <sledziewski.adam@proton.me>
2025-05-18 16:11:07 +0200

    Change to production drivers and set the finegrained to true

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -35,7 +35,7 @@
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -50,7 +50,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   # ----------- ENDING SECTION NVIDIA graphics ------------------
