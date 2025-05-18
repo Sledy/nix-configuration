9979f95e98000054db42b7a98ce324b2d1a50fde
Adam Śledziewski <sledziewski.adam@proton.me>
2025-05-18 16:01:19 +0200

    Change to vulkan beta drivers

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -31,7 +31,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = true;
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -50,7 +50,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 
   # ----------- ENDING SECTION NVIDIA graphics ------------------
