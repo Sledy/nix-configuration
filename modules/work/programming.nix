dad15e704325cf180e1acc516bdbd16b4af8a87a
Adam Śledziewski <sledziewski.adam@proton.me>
2025-06-24 14:55:18 +0200

    Flakes updated, docker configuration changed
    The problem with the docker was the link in the ./config/user directory which has not been cleaned by the nix garbage collection. Removal of docker sym link and the system rebuild do the work

--- modules/work/programming.nix
+++ modules/work/programming.nix
@@ -11,14 +11,21 @@
     python313Full # python 3.13
     python311Packages.pip # python pip for version 3.13
   ];
-  
-  
 
-  
-  virtualisation.docker.rootless = {
-  	enable = true;
-  	setSocketVariable = true;
-  };
+    virtualisation.docker = {
+      enable = false;
+
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+        # Optionally customize rootless Docker daemon settings
+        daemon.settings = {
+          dns = [ "1.1.1.1" "8.8.8.8" ];
+          registry-mirrors = [ "https://mirror.gcr.io" ];
+        };
+      };
+    };
+
   users.users.adam.extraGroups = [ "docker" ];
 }
 
